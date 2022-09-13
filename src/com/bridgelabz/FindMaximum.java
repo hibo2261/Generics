47bfc99caaa962d19a7d3a6bc0e587f15ba80361
hibo2261 <hborse1@gmail.com>
2022-09-13 16:11:47 +0530

    [Himanshu]:Added Find Maximum Of Using Generic Class

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,17 +1,37 @@
 package com.bridgelabz;
 
-public class FindMaximum {
+public class FindMaximum<T extends Comparable> {
 
+    T a;
+    T b;
+    T c;
 
-    public static Float toPrintMax(Float a ,Float b,Float c) {
+    //Constructor
+    public FindMaximum(T a, T b,T c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
 
-        Float max = a;
+    }
+
+    /**
+     * maximum numbers are found using generic methods
+     * @param a value is set to max
+     * @param b value is compared
+     * @param c value is  compared
+     * @return maximum is returned
+     */
+
+    public static <T extends Comparable<T>> T toPrintMax(T a ,T b,T c) {
+
+        T max = a;
 
         if(b.compareTo(max) > 0 )
             max = b;
 
         if(c.compareTo(max) > 0 )
             max = c;
+
         return max;
 
     }
--- src/com/bridgelabz/FindMaximumMain.java
+++ src/com/bridgelabz/FindMaximumMain.java
@@ -4,8 +4,9 @@ public class FindMaximumMain {
 
     public static void main(String[] args) {
 
-
-        System.out.println("The maximum value is -->"+ " "+FindMaximum.toPrintMax(2.2f, 1.35f, 60.2f));
+        System.out.println("The maximum value is"+ " " +FindMaximum.toPrintMax(67, 29, 40));
+        System.out.println("The maximum value is"+ " " +FindMaximum.toPrintMax(67.2f, 2.29f, 4.20f));
+        System.out.println("The maximum value is"+ " "+FindMaximum.toPrintMax("apple","peach","mango"));
 
     }
 
