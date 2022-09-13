8da992c54ab377927398fdc76e89d4628b1a6c54
hibo2261 <hborse1@gmail.com>
2022-09-13 15:52:09 +0530

    [Himanshu]:Added Find Maximum In Given Float.

--- src/com/bridgelabz/FindMaximumMain.java
+++ src/com/bridgelabz/FindMaximumMain.java
@@ -1,18 +1,12 @@
 package com.bridgelabz;
-/**
- *
- * Author : Himanshu Borse
- *
- */
-
 public class FindMaximumMain {
+
 
     public static void main(String[] args) {
 
 
-        System.out.println("The maximum value is"+ " "+FindMaximum.toPrintMax(92, 35, 60));
+        System.out.println("The maximum value is -->"+ " "+FindMaximum.toPrintMax(2.2f, 1.35f, 60.2f));
 
     }
 
 }
-
