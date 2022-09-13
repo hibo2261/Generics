d76d09a8ace50d7dc306414d7fa7626f176dea93
hibo2261 <hborse1@gmail.com>
2022-09-13 15:58:07 +0530

    [Himanshu]:Added Maximum In Given String

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -3,9 +3,10 @@ package com.bridgelabz;
 public class FindMaximum {
 
 
-    public static Float toPrintMax(Float a ,Float b,Float c) {
 
-        Float max = a;
+    public static String toPrintMax(String a ,String b,String c) {
+
+        String max = a;
 
         if(b.compareTo(max) > 0 )
             max = b;
